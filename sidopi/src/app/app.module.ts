6e0da0785fa57264437fd574439be44f85d5576a
maescalante <ma.escalante@uniandes.edu.co>
2020-10-25 15:39:55 -0500

    lksdfj

--- sidopi/src/app/app.module.ts
+++ sidopi/src/app/app.module.ts
@@ -5,16 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DonanteComponent } from './donante/donante.component';
 import { ReceptorComponent } from './receptor/receptor.component';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     DonanteComponent,
-    ReceptorComponent
+    ReceptorComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ sidopi/src/app/persona.ts
@@ -0,0 +1,16 @@
+export interface Persona{
+    nombre: string;
+    cedula: string;
+    tipoSangre: string;
+    colorPiel: string;
+  //  detalles del color de piel 
+    tatuajes: boolean;
+    cortes: boolean; 
+    cicatricres: boolean;
+    phPiel: number; 
+    cantidadVello: number; 
+    descripcion: string;   
+    donante: boolean;
+    receptor: boolean; 
+    foto: string;
+}
