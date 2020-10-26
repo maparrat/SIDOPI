088a09cf3a29df769a1ff524a38c36af9cd906a9
Miguel Armando Parra Tovar <ma.parrat@uniandes.edu.co>
2020-10-25 20:28:03 -0500

    ruta lr

--- sidopi/src/app/app-routing.module.ts
+++ sidopi/src/app/app-routing.module.ts
@@ -5,11 +5,15 @@ import { DonanteComponent } from './donante/donante.component';
 import { FormularioDonanteComponent } from './formulario-donante/formulario-donante.component';
 import { FormularioReceptorComponent } from './formulario-receptor/formulario-receptor.component';
 import { LandingComponent } from './landing/landing.component';
+import { ReceptorDetalleComponent } from './receptor-detalle/receptor-detalle.component';
+import { ReceptorComponent } from './receptor/receptor.component';
 
 const routes: Routes = [
   { path: 'fr', component: FormularioReceptorComponent,  pathMatch: 'full'}, 
   { path: 'fd', component: FormularioDonanteComponent, pathMatch: 'full' },
   { path: 'dd/:id', component: DonanteDetalleComponent, pathMatch:'full' },
+  { path: 'dr/:id', component: ReceptorDetalleComponent, pathMatch:'full' },
+  { path: 'lr', component: ReceptorComponent, pathMatch:'full' },
   { path: 'd', component: DonanteComponent, pathMatch: 'full' },
   { path: "", component: LandingComponent, pathMatch: 'full' },
   {path:"#", component:LandingComponent, pathMatch:'full'}
