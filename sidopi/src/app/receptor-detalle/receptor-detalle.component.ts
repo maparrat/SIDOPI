854c74b8c8be230cc398e2e8d992b5935f969851
maescalante <ma.escalante@uniandes.edu.co>
2020-10-26 08:00:58 -0500

    arreglos varios

--- sidopi/src/app/receptor-detalle/receptor-detalle.component.ts
+++ sidopi/src/app/receptor-detalle/receptor-detalle.component.ts
@@ -9,10 +9,10 @@ import { donadorService } from './donadorService';
   styleUrls: ['./receptor-detalle.component.css']
 })
 export class ReceptorDetalleComponent implements OnInit {
-  donante: Persona;
-  donante_id: number;
+  receptor: Persona;
+  receptor_id: number;
   personas: Persona[];
-  donnates:Persona[];
+  donantes:Persona[];
   compatibles:Persona[];
   constructor(private personaService:PersonaService, private route: ActivatedRoute ,private servicio:donadorService) { }
 
@@ -23,54 +23,45 @@ export class ReceptorDetalleComponent implements OnInit {
     
       .subscribe(donantes => {
         this.personas = donantes
-        console.log(this.donante_id)
+        console.log(this.receptor_id)
         this.personas.forEach(per => {
 
-          persona=>true== true ? this.personas.push(persona): 0
+          per.cedula == this.receptor_id.toString() ? this.receptor = per : 0
+          this.personas.push(per);
+
       });
-     
+        this.getCompatibles();
+        
     });
     
   }
-  //no funciona donantes
-  getJson(): void {
-    this.servicio.getDonantes()
-    
-    .subscribe(donantes => {
-      this.donnates = donantes
-      console.log(this.donante_id+"pru")
-      this.donnates.forEach(per => {
 
-        persona=>true== true ? this.donnates.push(persona): 0
-    });
-   
-  });
-}
 
   // xd aca donante 
   getCompatibles(): void{
-    for (let index = 1; index < 5; index++) {
-        const element = this.personas[index];
-        
-  
-        this.compatibles.push(element);
-          
-        
 
-      
-    }
+    this.personas.forEach(per => { 
+      let flag = true;
+      per.donante === true ? flag = false : 0;
+      per.tipoSangre != this.receptor.tipoSangre ? flag = false : 0;
+      per.phPiel != this.receptor.phPiel ? flag = false : 0;
+      per.cantidadVello - this.receptor.cantidadVello > 10 ? flag = false : 0;
+      per.colorPiel != this.receptor.colorPiel ? flag = false : 0;
+      per.cedula === this.receptor.cedula ? flag = false : 0;
+      flag === true ? this.compatibles.push(per) : 0;
+    })
   }
 
  
   ngOnInit(): void {
     this.personas = [];
     this.compatibles = [];
-    this.donante_id = +this.route.snapshot.paramMap.get('id');
-    this.donante = new Persona();
+    this.receptor_id = +this.route.snapshot.paramMap.get('id');
+    this.receptor = new Persona();
     
     this.getDetail();
     //this.getJson();
-    this.getCompatibles();
+    //this.getCompatibles();
     
 
   }
