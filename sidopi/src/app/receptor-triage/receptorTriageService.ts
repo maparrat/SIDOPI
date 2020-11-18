a9b1eab229a7b3487da47ebaf063dd023aea2346
TomasLangebaek <tomaslangebaek@gmail.com>
2020-11-17 19:42:53 -0500

    triage

---
+++ sidopi/src/app/receptor-triage/receptorTriageService.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Persona } from '../persona';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class receptorTriageService {
+  private apiUrl = "../../../assets/datos.json";
+    constructor(private http: HttpClient) { }
+
+
+  getDonantes(): Observable<Array<Persona>> {
+    return this.http.get<Array<Persona>>(this.apiUrl);
+  }
+
+}
+
